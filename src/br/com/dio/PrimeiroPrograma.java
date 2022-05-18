56bff8ff3a0e37fc4068c2612310f44a3adb7e82
Daniel Gullich <dang.akira@gmail.com>
2022-05-18 14:30:31 -0300

    Second Commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -10,7 +10,42 @@ public class PrimeiroPrograma {
         System.out.println("Hello World! " + (10+5));*/
 
         Gato gato = new Gato();
+        Livro livro = new Livro("O meu livro", 500);
         System.out.println(gato);
+        System.out.println(livro);
+    }
+}
 
+class Livro{
+    private String nome;
+    private Integer numeroPaginas;
+
+    public Livro(String nome, Integer numeroPaginas) {
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                '}';
     }
 }
