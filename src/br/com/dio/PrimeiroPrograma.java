369d4d486b5cd7cdd5ca68eee50396b762a8a8e7
Daniel Gullich <dang.akira@gmail.com>
2022-05-18 14:24:02 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        /*
+        int a = 10;
+        int b = 5;
+        System.out.println("Hello World! " + (10+5));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
